7b948a409cc036b3e93aeb824dceb051e5ca36fd
Alexander <aleneum@gmail.com>
2013-02-18 19:10:07 +0100

    initial checkin

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
-tomboy-addin-task
-=================
+Usage
+-----
 
-TomboyTask Addin
+Collects your tasks from all your Tomboy notes which comes in handy when you use Tomboy for Getting Things Done (GTD).
+Support contexts and ignores done tasks.
+
+* status: 0.1.0 (alpha)
+* tested with: Ubuntu 12.04 12.10
+* requires: tomboy libgtk2.0-cil-dev libmono-posix4.0-cil
+
+Mark and Collect Tasks
+----------------------
+
+* Create a bullet point within a Note and add "@Task".
+![create Note and @Task](img/ttask01.png)
+* Create your task list called "GTD". This one gets an additional tool called "Update Tasks".
+![create GTD](img/ttask02.png)
+* Update the list and have a look at your tasks, ordered by notebook.
+![update tasks](img/ttask03.png)
+
+One Task per Note
+-----------------
+* TomboyTask ignores tasks your striked out.
+![list of tasks](img/ttask04.png)
+* if your note contains more than one task only the first not finished task is shown
+![gtd list](img/ttask05.png)
+
+Use Contexts
+------------
+* add a context to specify tasks.
+![advanced task](img/ttask06.png)
+* now you can create a specific task list called "GTD.advanced" to show only tasks for this context.
+![advanced task](img/ttask07.png)
+* The generic "GTD" note will collect all tasks, disregarding the context
+![advanced task](img/ttask08.png)
+
+Installation Instructions
+-------------------------
+
+
+dmcs -debug -out:TomboyTask.dll -target:library TomboyTaskNoteAddin.cs -res:TomboyTask.addin.xml -pkg:gtk-sharp-2.0,tomboy-addins -r:/usr/lib/mono/4.0/Mono.Posix.dll
+
+~/.config/tomboy/addins/
+
+https://live.gnome.org/Tomboy/PluginList
---
+++ TomboyTaskNoteAddin.cs
@@ -0,0 +1,98 @@
+using System;
+using Mono.Unix;
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Tomboy.TomboyTask
+{
+	public class TomboyTaskNoteAddin : NoteAddin
+	{
+
+		Gtk.MenuItem item;
+		string[] categories;
+		
+		public override void Initialize (){
+		}
+
+		public override void Shutdown (){
+		}
+		
+		public override void OnNoteOpened ()
+		{
+			if (this.Note.Title.StartsWith("GTD")) {
+				item = new Gtk.MenuItem("Update Tasks");
+				item.Activated += OnMenuItemActivated;
+				item.AddAccelerator ("activate", Window.AccelGroup,
+					(uint) Gdk.Key.d, Gdk.ModifierType.ControlMask,
+					Gtk.AccelFlags.Visible);
+				item.Show ();
+				AddPluginMenuItem (item);
+			}
+		}
+
+		void OnMenuItemActivated (object sender, EventArgs args)
+		{
+			if (this.Note.Title.Contains(".")) {
+				string cats = this.Note.Title.Substring(4,this.Note.Title.Length-4);
+				if (cats.Contains(".")) {
+					categories = cats.Split('.');
+				} else {
+					categories = new string[1] {cats};
+				}
+				
+			} else {
+				categories = new string[0];
+			}
+			this.updateContent();
+		}
+		
+		private void updateContent() {
+			string content = string.Format("<note-content version=\"0.1\">{0}\n\n\n",this.Note.Title);
+			SortedDictionary<string, string> notebook_map = new SortedDictionary<string, string>();
+//			SortedDictionary<DateTime, string> timed_events = new SortedDictionary<DateTime, string>();
+			foreach (Note note in this.Manager.Notes) {
+				if (note.Title.StartsWith("GTD")) continue;
+				string[] lines = note.XmlContent.Split('\n');
+				string task = "";
+				foreach (string line in lines) {
+					if (line.Contains("<strikethrough>")) continue;
+					int idx = line.IndexOf("@Task");
+					if (idx < 0) continue;
+					bool has_cat = false;
+//					DateTime dateTime = null;
+					string cats = line.Substring(idx,line.Length-idx);
+					foreach(string cat in categories) {
+//						if (DateTime.TryParse(input, out dateTime)) {
+//							
+//						}
+						if (cats.Contains(cat)) has_cat = true;
+					}
+					if (!has_cat) continue;
+					task = line.Substring(0,idx-1);
+					Regex rgx = new Regex("<[^>]*>");
+					task = rgx.Replace(task,"");
+					break;
+				}
+				if (task.Length < 1) continue;
+				
+				Notebooks.Notebook book = Notebooks.NotebookManager.GetNotebookFromNote(note);
+				string book_name = "No notebook";
+				if (book != null) {
+					book_name = Notebooks.NotebookManager.GetNotebookFromNote(note).Name;
+				}
+				if (!notebook_map.ContainsKey(book_name)) {
+					notebook_map[book_name] = "";
+				}
+				task = string.Format("{0} ({1})",task,note.Title);
+				notebook_map[book_name] += string.Format("<list-item dir=\"ltr\">{0}\n</list-item>", task);
+			}
+			foreach (string book_name in notebook_map.Keys) {
+				content += string.Format("<size:large>{0}</size:large>\n<list>{1}</list>\n\n", book_name, notebook_map[book_name]);
+			}
+			content += "</note-content>";
+			//Console.WriteLine(content);
+			this.Note.XmlContent = content;
+		}
+	}
+}
